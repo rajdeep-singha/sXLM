090c1c3d6daad411f588c81877889ca469e419af
Rajdeep <rajdeepsingha384@gmail.com>
2026-02-20 16:03:48 +0530

    feat: staking-pool written with our sxlm minting and burning logic

--- contracts/staking-pool/src/error.rs
+++ contracts/staking-pool/src/error.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+
+    AlreadyInitialized = 1,
+    
+    InvalidAmount = 2,
+    
+    InsufficientBalance = 3,
+    
+    ContractPaused = 4,
+    
+    Unauthorized = 5,
+    
+    InvalidExchangeRate = 6,
+    
+    CrossContractCallFailed = 7,
+    
+    InvalidAddress = 8,
 }
